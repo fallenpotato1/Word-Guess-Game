d2328bcfb449dfd18f2693d81650fb5b61ec2ace
fallen potato <cruzjon32@gmail.com>
2018-10-22 16:56:53 -0700

    finsihed the work pushing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,43 @@
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-            'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-            't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-
-        var livesLeft = document.getElementsByClassName("numberOfWins")
-        var theAnswer = document.querySelector(".answer")
-        var guessesRemaining = document.getElementsByClassName("guesses")
-        var lettersGuessed = document.getElementsByClassName("lettersGuessed")
-
-        var games = ["battlefield", "fallout", "skyrim", "rocket league", "call of duty",
+var games = ["battlefield", "fallout", "skyrim", "rocket league", "call of duty",
             "pubg", "overwatch", "mario", "god of war"]
 
-        var randomGame = games[Math.floor(Math.random() * games.length)]
-        var addElement = document.createElement("h2")
-        var chars = randomGame.split("")
+        var theAnswer = document.querySelector(".answer")
 
-        for (letter = 0; letter < chars.length; letter++) {
-            chars[letter] = "-"
+        function randomWord() {
+            var randomGame = games[Math.floor(Math.random() * games.length)]
+            return randomGame
         }
 
-        var joinedHidden = chars.join("")
-
-        addElement.textContent = joinedHidden
-        addElement.classList.add("iHopeThisWorks")
-        theAnswer.appendChild(addElement)
+        var thisWord = randomWord()
+        
 
 
+        function startGame() {
+            for (letters = 0; letters < thisWord.length; letters++) {
+                var createHidden = document.createElement("h2")
+                createHidden.setAttribute("class", "iHopeThisWorks")
+                createHidden.setAttribute("value", thisWord[letters])
+                createHidden.textContent = "-"
+                theAnswer.appendChild(createHidden)
+            }
+        }
+        startGame()
+        
 
         function checker(event) {
-            var theWord = Array.from(randomGame).join("")
             var theCode = event.charCode
             var stringCode = String.fromCharCode(theCode)
-            var changingBack = document.querySelector(".iHopeThisWorks")
+            var changingBack = document.querySelector
             var theHidden = changingBack.textContent
-            var hiddenArray = Array.from(theHidden).join("")
-            var letterPosition = theWord.indexOf(stringCode)
-            
 
 
-            console.log(stringCode)
-            console.log(theWord)
-            console.log(hiddenArray)
-            console.log(theWord.length)
-            
-            var newArray = []
-            var timesTried = 0
-            var correct = theWord.length
-            var whichCorrect
-            
-            
-                
-            if (stringCode === theWord[letterPosition]) {
-                for(theOne = correct.length; theOne > correct.length; theOne--) {
-                    if(theOne !== letterPosition) {
-                        newArray[theOne] =  "-"
-                    }
+
+            for(eachLetter = 0; eachLetter <= thisWord.length; eachLetter++) {
+                var theTag = document.getElementsByClassName("iHopeThisWorks")[eachLetter]
+                if (stringCode === thisWord[eachLetter]) {
+                   theTag.textContent = thisWord[eachLetter]
                 }
 
-                delete newArray[letterPosition]
-                newArray[letterPosition] = theWord[letterPosition]
-            
-            } 
-            var thisIsTooMuch = newArray.join("")
-            changingBack.textContent = thisIsTooMuch
-            
-                    
-                    
-                    // var theFinal = hiddenArray.splice(letterPosition, 1, theWord[letterPosition])
-
-                // console.log(hiddenArray.replace(hiddenArray[letterPosition], theWord[letterPosition]))
-                }
-
+            }
+        }
 
